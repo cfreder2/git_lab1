c7273695b3b47bcca6d774234b7f29f288b7ec67
Chris F <cfreder2@nd.edu>
2021-12-06 22:12:49 -0500

    added validate email

---
+++ validate_email.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def validate_email(email):
+    """ Ensure that emails are valid
+    
+    Arguments:
+    email - A pandas series of emails
+
+    Return:
+    A boolean pandas series.  True means valid.  False means invalid.
+    """
+
+    bool_email = email.str.contains(r"[A-z0-9]+@[A-z0-9.]+.(edu|com)")
+    return bool_email
+
+emails = pd.Series(['cfreder2@nd.edu','happyday@gmail.com', '1123 main street'])
+print(validate_email(emails))
