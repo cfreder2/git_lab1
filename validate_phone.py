c0d63ea48ddc6a3f4e3d2dad7c4d11b2b69f4b46
Chris F <cfreder2@nd.edu>
2021-12-06 21:49:50 -0500

    Stubbed out validate phone function

---
+++ validate_phone.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def validate_phone(phone_number):
+    """
+    Ensure that phone numbers are valid
+
+    Arguments:
+    phone_number: A pandas sereis of phone numbers
+
+    Return:
+    A boolean pandas series. True is valid. False is invalid
+    """
+    return True
+
+
+print(validate_phone(True))
