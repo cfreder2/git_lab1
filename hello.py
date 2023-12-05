e3f9bbb8959f1dccdf98574a8dbd6808a8536f06
Chris F <cfreder2@nd.edu>
2023-12-04 21:43:39 -0500

    added languages

--- hello.py
+++ hello.py
@@ -1,2 +1,20 @@
+import random
 print("Hi class!")
-print("This is fun, what a great second day")
+print("This is fun?, what a great second day")
+
+def print_hello():
+    languages = ["English", "Spanish", "French", "German", "Italian"]
+    hello_messages = {
+        "English": "Hello",
+        "Spanish": "Hola",
+        "French": "Bonjour",
+        "German": "Guten Tag",
+        "Italian": "Ciao"
+    }
+    
+    random_language = random.choice(languages)
+    hello_message = hello_messages[random_language]
+    
+    print(f"{hello_message}, {random_language}!")
+
+print_hello()
